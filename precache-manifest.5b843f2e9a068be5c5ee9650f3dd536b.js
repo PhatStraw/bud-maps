b834ce79007141a40efc4e6fd8b5ef276b4b79c3
Kevin Sims <kevinsims@Not-Yours.attlocal.net>
2023-03-08 12:18:03 -0800

    Updates

---
+++ precache-manifest.5b843f2e9a068be5c5ee9650f3dd536b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61609a53afb16a67103c8c89c9420ec6",
+    "url": "/bud-maps/index.html"
+  },
+  {
+    "revision": "cd861442fb5e0d3acb26",
+    "url": "/bud-maps/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "010035e47fa28a74d90d",
+    "url": "/bud-maps/static/css/4.0309ab85.chunk.css"
+  },
+  {
+    "revision": "321d21b60192ba3ec7e0",
+    "url": "/bud-maps/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "2209c4fc9484c132d708",
+    "url": "/bud-maps/static/css/7.751a6c49.chunk.css"
+  },
+  {
+    "revision": "5fe859b8b2d1ebc87788",
+    "url": "/bud-maps/static/css/main.162c7196.chunk.css"
+  },
+  {
+    "revision": "cd861442fb5e0d3acb26",
+    "url": "/bud-maps/static/js/0.3afe3e9c.chunk.js"
+  },
+  {
+    "revision": "f77df77b0064a06a0ced",
+    "url": "/bud-maps/static/js/3.c014426f.chunk.js"
+  },
+  {
+    "revision": "010035e47fa28a74d90d",
+    "url": "/bud-maps/static/js/4.de316aa2.chunk.js"
+  },
+  {
+    "revision": "321d21b60192ba3ec7e0",
+    "url": "/bud-maps/static/js/5.31942fad.chunk.js"
+  },
+  {
+    "revision": "7d90f731adb96c7b86e3",
+    "url": "/bud-maps/static/js/6.91934930.chunk.js"
+  },
+  {
+    "revision": "2209c4fc9484c132d708",
+    "url": "/bud-maps/static/js/7.05f91092.chunk.js"
+  },
+  {
+    "revision": "5fe859b8b2d1ebc87788",
+    "url": "/bud-maps/static/js/main.8774e132.chunk.js"
+  },
+  {
+    "revision": "bfcb4dcf47f2d76e7121",
+    "url": "/bud-maps/static/js/runtime~main.92cc50db.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/bud-maps/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/bud-maps/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/bud-maps/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/bud-maps/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
